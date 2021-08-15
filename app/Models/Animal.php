b65ee84f4976653d457ab680a60ac074ac8177fe
Natã Romão Ferreira <nataromao@pop-os.localdomain>
2021-08-15 11:28:12 -0300

    Import do bootstrap + testes

---
+++ app/Models/Animal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Animal extends Model
+{
+
+    protected $table = 'Animal';
+
+    protected $timestamps = false;
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', compact('teste'));
 });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
