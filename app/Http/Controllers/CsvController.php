52572050d80d68a9b9c76003e7034b57600f0482
Natã Romão Ferreira <nataromao@pop-os.localdomain>
2021-08-15 21:16:41 -0300

    Controller de relatorios com gerações segmentadas

---
+++ app/Http/Controllers/CsvController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Animal;
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+use League\Csv\Writer;
+
+class CsvController extends Controller
+{
+
+    public function getRelatorioCliente()
+    {
+
+        $this->gerarRelatorioCompleto(Cliente::all(), [
+            'Id',
+            'Nome',
+            'Telefone1',
+            'Telefone2',
+            'Email'
+        ]);
+
+        return true;
+    }
+
+    public function getRelatorioAnimal()
+    {
+        $this->gerarRelatorioCompleto(Animal::all(), [
+            'Id',
+            'IdCliente',
+            'Raca',
+            'Nome',
+            'Especie',
+            'HistoricoClinico',
+            'Nascimento'
+        ]);
+
+        return true;
+    }
+
+    private function gerarRelatorioCompleto($dados, $colunas)
+    {
+        $dados = json_decode($dados);
+        $csv   = Writer::createFromString("");
+        $csv->insertOne($colunas);
+
+        foreach($dados as $campos) {
+            $linha = [];
+
+            foreach ($campos as $campo) {
+                $linha[] = $campo;
+            }
+
+            $csv->insertOne($linha);
+        }
+
+        $idUnico = uniqid();
+        $csv->output("relatorio_{$idUnico}.csv");
+    }
+}
