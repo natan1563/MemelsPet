9bdf64b67c3725fa430d51c5ffa816925ea271e5
Natã Romão Ferreira <nataromao@pop-os.localdomain>
2021-08-17 20:35:14 -0300

    Cria model da tabela Racas

---
+++ app/Models/Racas.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Racas extends Model
+{
+    use HasFactory;
+
+    protected $table    = 'especies';
+
+    protected $fillable = ['id', 'especie_id', 'nome'];
+
+    public $timestamps   = false;
+
+}
